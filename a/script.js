56bc6cebc4b40077dacb8c79a5b8a982a819d5d3
kathund <jacobbray2007@gmail.com>
2022-09-20 15:08:36 +0800

    Create script.js

---
+++ a/script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+    var bill = document.getElementById("bill").value;
+    var people = document.getElementById("people").value;
+    final = bill * 1.2
+    final = final - bill
+    final = final / people
+    alert(final);
+} 
