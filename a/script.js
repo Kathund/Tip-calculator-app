5059b5f3225d34c4bff104d93c031f516f0c1a78
kathund <jacobbray2007@gmail.com>
2022-09-24 18:15:39 +0800

    Restart

--- a/script.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-    var bill = document.getElementById("bill").value;
-    var people = document.getElementById("people").value;
-    final = bill * 1.2
-    final = final - bill
-    final = final / people
-    alert(final);
-} 
