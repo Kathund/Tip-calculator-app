578d8fd3be3d37249ff809b1a65d2ed233daeada
kathund <jacobbray2007@gmail.com>
2022-10-13 08:37:29 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,54 +1,38 @@
-// get input from user under the id of "bill"
-var bill = document.getElementById("bill").value;
-// get input from user under the id of "tip"
-var tip = document.getElementById("tip").value;
-// get input from user under the id of "people"
-var people = document.getElementById("people").value;
+// get user input bill
+var bill = document.getElementById("bill");
+// get user input tip
+var tip = document.getElementById("tip");
+// get user input people
+var people = document.getElementById("people");
 
-// then log the data to console
-console.log(bill, tip, people);
+// console log user input
+console.log("bill: " + bill);
+console.log("tip: " + tip);
+console.log("people: " + people);
 
-// work out the tip
-function calculateTip() {
-    // work out the tip
-    var tipAmount = (bill * tip) / 100;
-    // work out the total
-    var total = bill + tipAmount;
-    // work out the tip per person
-    var tipPerPerson = tipAmount / people;
-    // work out the total per person
-    var totalPerPerson = total / people;
-    // display the tip per person
-    document.getElementById("tipPerPerson").innerHTML = tipPerPerson;
-    // disaply the total per person
-    document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
-}
 
-// funcation reset
-// clear the input fields
-function reset() {
-    // document.getElementById("bill").value = "";
-    // document.getElementById("tip").value = "";
-    // document.getElementById("people").value = "";
-    // get input from user under the id of "bill"
-    var bill = document.getElementById("bill").value;
-    // get input from user under the id of "tip"
-    var tip = document.getElementById("tip").value;
-    // get input from user under the id of "people"
-    var people = document.getElementById("people").value;
-    // work out the tip
-    var tipAmount = (bill * tip) / 100;
-    // work out the total
-    var total = bill + tipAmount;
-    // work out the tip per person
-    var tipPerPerson = tipAmount / people;
-    // work out the total per person
-    var totalPerPerson = total / people;
-    // console log all the data
-    console.log(bill, tip, people, tipAmount, total, tipPerPerson, totalPerPerson);
-    // display the tip per person
-    document.getElementById("tipPerPerson").innerHTML = tipPerPerson;
-    // disaply the total per person
-    document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
+// check for update on user input
+// then update the vars
+bill.addEventListener("input", function() {
+    bill = document.getElementById("bill").value;
+    update();
+    }
+);
+tip.addEventListener("input", function() {
+    tip = document.getElementById("tip").value;
+    update();
+    }
+);
+people.addEventListener("input", function() {
+    people = document.getElementById("people").value;
+    update();
+    }
+);
 
+// on user input update
+// log to console
+function update() {
+    console.log("bill: " + bill);
+    console.log("tip: " + tip);
+    console.log("people: " + people);
 }
