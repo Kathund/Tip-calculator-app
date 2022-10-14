114a5d2d9aecc5c9cb435b417ddf3cddf6334f8f
kathund <jacobbray2007@gmail.com>
2022-10-14 13:31:20 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,86 +1,48 @@
-// get user input bill
-var bill = document.getElementById("bill");
-// get user input tip
-var tip = document.getElementById("tip");
-// get user input people
-var people = document.getElementById("people");
-
-// console log user input
-console.log("bill: " + bill);
-console.log("tip: " + tip);
-console.log("people: " + people);
 
 
-// check for update on user input
-// then update the vars
-bill.addEventListener("input", function() {
-    bill = document.getElementById("bill").value;
-    // update the variables
-    update();
-    // update the display
-    updateDisplay();
-    }
-);
-tip.addEventListener("input", function() {
-    tip = document.getElementById("tip").value;
-    // update the variables
-    update();
-    // update the display
-    updateDisplay();
-    }
-);
-people.addEventListener("input", function() {
-    people = document.getElementById("people").value;
-    // update the variables
-    update();
-    // update the display
-    updateDisplay();
-    }
-);
+// pull the user info from the user input sections
+// bill
+var bill = document.getElementById("bill").value;
+// tip
+var tip = document.getElementById("tip").value;
+// people
+var people = document.getElementById("people").value;
 
-// on user input update
-// log to console
-function update() {
-    console.log("bill: " + bill);
-    console.log("tip: " + tip);
-    console.log("people: " + people);
-}
+// console log the data
+console.log(`bill: ${bill}`);
+console.log(`tip: ${tip}`);
+console.log(`people: ${people}`);
 
-// work out the tip
-// log into variables
-var tipAmount = bill * (tip / 100);
-var total = bill + tipAmount;
-var tipPerPerson = tipAmount / people;
-var totalPerPerson = total / people;
 
-// log the variables
-console.log("tipAmount: " + tipAmount);
-console.log("total: " + total);
-console.log("tipPerPerson: " + tipPerPerson);
-console.log("totalPerPerson: " + totalPerPerson);
+// calculate the tip
+// calculate the total
+var total = tip + bill
+// calculate the tip per person
+var tipPerPerson = tip / people;
+// calculate the total per person
+var totalPerPerson = (bill / people) + tipPerPerson;
 
-// on load
-// update the display
-window.onload = function() {
-    // update the display
-    document.getElementById("tipPerPerson").innerHTML = tipPerPerson;
-    document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
-}
+
+// console log the data
+console.log(`tipPerPerson: ${tipPerPerson}`);
+console.log(`totalPerPerson: ${totalPerPerson}`);
+
+// check if there is an update on user input
+// bill
+document.getElementById("bill").addEventListener("input", updateDisplay());
+var bill = document.getElementById("bill").value;
+// tip
+document.getElementById("tip").addEventListener("input", updateDisplay());
+var tip = document.getElementById("tip").value;
+// people
+document.getElementById("people").addEventListener("input", updateDisplay());
+var people = document.getElementById("people").value;
 
 // update the display
+// using a function to update the display
 function updateDisplay() {
-    // update the display
-    document.getElementById("tipPerPerson").innerHTML = tipPerPerson;
-    document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
-}
-
-// reset
-function reset() {
-    // reset the user input
-    document.getElementById("bill").value = "";
-    document.getElementById("tip").value = "";
-    document.getElementById("people").value = "";
-    // reset the display
-    document.getElementById("tipPerPerson").innerHTML = 0;
-    document.getElementById("totalPerPerson").innerHTML = 0;
+    // update the tip per person
+    document.getElementById("tipPerPerson").innerHTML = tipPerPerson
+    // update the total per person
+    document.getElementById("totalPerPerson").innerHTML = totalPerPerson
 }
