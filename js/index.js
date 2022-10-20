76c94575980a08b035c274d34360c9cedde2b44d
kathund <jacobbray2007@gmail.com>
2022-10-20 17:40:54 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,57 +1,26 @@
-
-// TODO fix calculate tip function
-// TODO fix display not updating on update
-
-// pull the user info from the user input sections
-// bill
-var bill = document.getElementById("bill").value;
-// tip
+// get tip info from the input
 var tip = document.getElementById("tip").value;
-// people
+// get bill info from the input
+var bill = document.getElementById("bill").value;
+// get number of people info from the input
 var people = document.getElementById("people").value;
 
-// console log the data
-console.log(`bill: ${bill}`);
-console.log(`tip: ${tip}`);
-console.log(`people: ${people}`);
-
-
-// use a function to calculate the tip
+// using a function
+// calculate the tip
 function calculateTip() {
-    // get values from the user input
-    // bill
-    var bill = document.getElementById("bill").value;
-    // tip
-    var tip = document.getElementById("tip").value;
-    // people
-    var people = document.getElementById("people").value;
-    // calculate the tip
-    // calculate the total
-    var total = tip + bill
-    // calculate the tip per person
+    // get the total amount
+    var totalAmount = bill + tip
+    // get the total amount per person
+    var totalPerPerson = totalAmount / people
+    // get the tip per person
     var tipPerPerson = tip / people
-    // calculate the total per person
-    var totalPerPerson = total / people
-    // console log the data
-    console.log(`total: ${total}`);
-    console.log(`tip per person: ${tipPerPerson}`);
-    console.log(`total per person: ${totalPerPerson}`);
 }
 
-
-// console log the data
-console.log(`tipPerPerson: ${tipPerPerson}`);
-console.log(`totalPerPerson: ${totalPerPerson}`);
-
-// update the display
-// using a function to update the display
-function updateDisplay() {
-    calculateTip();
-    // 1000 miliseconds timeout
-    setTimeout(function() {
-        // update the tip per person
-        document.getElementById("tipPerPerson").innerHTML = tipPerPerson
-        // update the total per person
-        document.getElementById("totalPerPerson").innerHTML = totalPerPerson
-    }, 1000);
+// using a funtion
+// display the tip
+function displayTip() {
+    // get the element
+    var tipElement = document.getElementById("tipPerPerson");
+    // set the tip per person
+    tipElement.innerHTML = tipPerPerson;
 }
